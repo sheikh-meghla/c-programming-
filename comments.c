06f181794f76d02af6c6b57ec1d0809cc3aaa9ff
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-09 11:25:23 +0600

    Update comments in comments.c for clarity and detail

--- comments.c
+++ comments.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    // This is a comment in C programming
+    // This is a single line comment
+
+    /*
+     This is a multi-line comment
+     It can span multiple lines
+     It is useful for providing detailed explanations or commenting out blocks of code
+    */
     printf("Hello World");
     return 0;
 }
