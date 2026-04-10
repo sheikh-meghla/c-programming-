c405c861070557b4576957667d6eaccd8fb71978
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-10 20:06:08 +0600

    nested_if create

---
+++ nested_if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int x = 20;
+  int y = 70;
+
+  if (x > 13) {
+    printf("x is greater than 13\n");
+
+    if (y > 20) {
+      printf("y is also greater than 20\n");
+    }
+  }
+
+  return 0;
+}
