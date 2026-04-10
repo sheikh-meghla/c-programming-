98dcd0e9c9995ff4660edb519cb58d8b4b5a5839
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-10 09:09:06 +0600

    else if .c create

---
+++ else_if.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+    int tk;
+    scanf("%d", &tk);
+
+
+    if (30 > tk && tk > 20)
+    {
+        printf("ruti khabo");
+    }
+     else if (tk > 80)
+    {
+        printf("bargar khabo");
+    }
+     else
+    {
+        printf("ruti khabo na");
+    }
+}
