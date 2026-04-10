9c24b5d3ae58ee6c239b8578c17c3bcf8e6fe169
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-10 08:02:17 +0600

    Refactor code structure for improved readability and maintainability

---
+++ extended.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int normalInt = 10;
+    long longInt = 10000000000;
+    short shortInt = 5;
+    unsigned int unsignedInt = 20;
+    printf("Normal Integer: %d\n", normalInt);
+    printf("Long Integer: %lld\n", longInt);
+    printf("Short Integer: %d\n", shortInt);
+    printf("Unsigned Integer: %u\n", unsignedInt);
+    return 0;
+
+}
