73728e3e31ffca73c1cb4291fb5f65eb15eaa28c
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-12 22:14:32 +0600

    all

--- test.c
+++ test.c
@@ -4,7 +4,7 @@ int main(){
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
         if(n%i==0){
-            printf("%d/n",i);
+            printf("%d\n",i);
         }
         
     }
---
+++ trivujer_khetrofol.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    scanf("%d %d",&a,&b);
+    float khetrofol = .5*a*b;
+    printf("khetrofol holo = %.01f",khetrofol);
+    return 0;
+}
