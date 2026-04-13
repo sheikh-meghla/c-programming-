9239563cd51eadb2a58ace2f6220e80392d839b6
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-13 20:18:30 +0600

    string length and size

---
+++ length_and_size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+  char alphabet[] = "MEGHLA";
+  printf("Length is: %zu\n", strlen(alphabet));
+  printf("Size is: %zu\n", sizeof(alphabet));
+  return 0;
+}
