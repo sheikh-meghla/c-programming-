b2f764d100fa221819f3b5ae2f82d68977058932
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-15 21:09:03 +0600

    Refactor array sorting and update output for second largest number

--- second_largest_num.c
+++ second_largest_num.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
 int main(){
     int num[]={1,2,3,4,5,23,6,7,8,9,0};
-    int sort_num[11];
-    for(int i=0;i<11;i++){
-        if (i<num[1])
-        {
-            
-        }
-        
+    for(int i=0;i<10;i++){
+        if(num[i] %2 ==0)
+            printf("%d\n",num[i]);
     }
     
 }
--- swap_number.c
+++ swap_number.c
@@ -1,14 +1,34 @@
 #include<stdio.h>
 int main(){
-    int a = 11;
-    int b = 22;
-    printf("%d %d\n",a,b);
-    int c = a;
 
-     a = b;
+    int array[]={4,2,68,9,0,200};
 
-     b = c;
+    for (int i = 0; i < 5; i++){
+        for (int j = i+1; j < 5; j++){
 
-    printf("%d %d\n",a,b);
+            if (array[i] > array[j]) {
 
+                int tamp = array[i];
+
+                array[i]=array[j];
+
+                array[j]=tamp;
+            } 
+        }
+
+    }    
+    
+    for(int i=0;i<5;i++){
+        printf("%d ",array[i]);
+    }  
+
+    int len = sizeof(array) / sizeof(array[0]);
+
+    printf("\n array len = %d\n",len);
+
+    printf("second large num =  %d",array[len-2]);
+
+    
+    
+    
 }
