6fb26b033c64b64a6cf595eb91c1de52391f0751
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-16 19:50:12 +0600

    Add a C program that greets users and update output file

---
+++ functions_param.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void functions(char name[]){
+    printf("hello %s\n",name);
+
+}
+int main(){
+    functions("Liza");
+    functions("Rafi");
+    functions("Nurani Tasnim");
+    return 0;
+}
