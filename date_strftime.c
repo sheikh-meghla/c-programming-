fff61d11ab9c478f142c99290b6cdf6df99ceaca
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-18 15:51:10 +0600

    Add date_strftime.c to format and display the current date and time

---
+++ date_strftime.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<time.h>
+
+int main(){
+time_t now = time (NULL);
+struct tm *t = localtime(&now);
+char buffer[80];
+
+
+strftime(buffer, sizeof(buffer), "%d-%m-%Y %H:%M:%S", t);
+printf("Formatted time: %s\n", buffer);
+
+
+    return 0;
+}
