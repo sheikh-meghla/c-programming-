9b7e72ffac1ffe69a5bd2bc63135d1692de39daa
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-20 20:26:06 +0600

    Add variables_reallife.c to demonstrate variable usage and output student details; update output.txt accordingly

---
+++ variables_reallife.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int studentID = 15;
+  int studentAge = 25;
+  float studentFee = 75.25;
+  char studentGrade = 'A';
+
+  printf("Student id: %d\n", studentID);
+  printf("Student age: %d\n", studentAge);
+  printf("Student fee: %f\n", studentFee);
+  printf("Student grade: %c", studentGrade);
+
+  return 0;
+}
