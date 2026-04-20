0399ca2cbd8e15291ab70eece8351ce601807306
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-20 21:50:53 +0600

    Add boolean_if_else.c to demonstrate voting eligibility; update output.txt with result

---
+++ boolean_if_else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int age = 28;
+    int votingage = 18;
+
+    if(age >= votingage){
+        printf("Old enough to vote!");
+    }else{
+        printf("Not old enough to vote.");
+    }
+
+
+    return 0;
+
+}
