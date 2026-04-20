451eb45dea3ff0ceac9631d6e331e7b48feef372
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-20 22:00:53 +0600

    if practical doorcode

---
+++ if_practical_doorcode.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+  int doorCode = 4536;
+
+  if (doorCode == 4536) {
+    printf("Correct code.\nThe door is now open.");
+  } else {
+    printf("Wrong code.\nThe door remains closed.");
+  }
+
+  return 0;
+}
