04149b727bfaeb270086f1f581238b0ed4312c71
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-21 20:13:46 +0600

    Add while_loop_reallife.c to demonstrate countdown using a while loop; include compiled while_loop_reallife.exe

---
+++ while_loop_reallife.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  int countdown = 4;
+
+  while (countdown > 0) {
+    printf("%d\n", countdown);
+    countdown--;
+  }
+
+  printf("oohhhhhhhh!!\n");
+
+  return 0;
+}
