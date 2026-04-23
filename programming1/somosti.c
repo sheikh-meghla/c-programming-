c2fe4ebee34e5da8d9e3e890a2b2aa6cf487ffd2
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-23 21:53:45 +0600

    Add somosti.c to calculate the sum of the first n integers; include compiled somosti.exe and update output.txt

---
+++ programming1/somosti.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int sum = 0;
+    int n;
+    scanf("%d",&n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    printf("%d",sum);
+
+    return 0;
+    
+}
