6df99cff0a8378a3f2c0f4f7304612d6411d044f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-04-24 23:00:46 +0600

    Add somosti1.c to calculate the sum of an arithmetic series; include compiled somosti1.exe and update input.txt and output.txt with new values

---
+++ programming1/somosti1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    int a;
+    int d;
+    int n;
+    scanf("%d %d %d",&a, &d, &n);
+    int somosti = n/2*(2*a+(n-1)*d);
+    printf("%d",somosti);
+
+
+    return 0;
+}
