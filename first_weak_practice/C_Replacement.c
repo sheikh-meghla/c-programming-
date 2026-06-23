d0e047f8868ccd5b7e109f3d4cb4a1fc8bded835
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-23 08:55:58 +0600

    Add C. Replacement program to process an array of integers

---
+++ first_weak_practice/C_Replacement.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+
+    int a[n];
+    // input nilam
+    for (int i = 0; i < n; i++)
+    {
+       scanf("%d",&a[i]);
+       
+    }
+    // logic
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i]<0){
+           a[i]=2;
+
+        }else if(a[i] > 0){
+
+            a[i]=1;
+        }else if(a[i]==0){
+            a[i]=0;
+            
+        }
+
+        
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+    
+
+    
+}
