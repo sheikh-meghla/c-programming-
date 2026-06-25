c341d43bdba4d2749f8787b8f8946ee7b8cbd66d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-25 17:11:21 +0600

    Add implementation for replacing minimum and maximum values in an array

---
+++ first_weak_practice/M_Replace_MinMax.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    int a[n];
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    int min = 0;
+    int max = 0;
+
+    for(int i = 1; i < n; i++)
+    {
+        if(a[i] < a[min])
+        {
+            min = i;
+        }
+
+        if(a[i] > a[max])
+        {
+            max = i;
+        }
+    }
+
+    int temp = a[min];
+    a[min] = a[max];
+    a[max] = temp;
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
