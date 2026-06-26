d885f6ba1a0337f66fa38143b9bf603e77785fb5
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-26 13:38:58 +0600

    Add implementation for length of a string in C

---
+++ first_weak_practice/length-of-a-string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    char s[100];
+    scanf("%s",&s);
+    int count;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        count ++;
+    }
+    printf("%d",count);
+    
+
+    return 0;
+}
