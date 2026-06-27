bf0d4ecac68fbeb193dd4a024f10ffc5cf1ba45b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-27 08:27:27 +0600

    Add string copy program using strcpy in string-copy-using-strcpy.c

---
+++ first_weak_practice/string-copy-using-strcpy.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    char a[101], b[101];
+    scanf("%s %s",&a, &b);
+    int len = strlen(b);
+    
+    strcpy(a,b);
+
+    printf("%s %s",a, b);
+
+    return 0;
+}
