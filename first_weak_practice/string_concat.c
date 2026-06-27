5919b779a0724b0a7193dceba2ae57391386a51f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-27 08:46:54 +0600

    Add string concatenation program in C
    - Implemented a simple C program that concatenates two strings input by the user.
    - Utilized standard input/output functions and string manipulation functions from string.h.
    - The program reads two strings, calculates their lengths, and appends the second string to the first.

---
+++ first_weak_practice/string_concat.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    char a[101], b[101];
+    scanf("%s %s",&a, &b);
+
+    int len_a = strlen(a);
+    int len_b = strlen(b);
+
+    for (int i = 0; i < len_b; i++)
+    {
+        a[i+len_a] = b[i];
+    }
+    
+    
+    printf("%s %s",a, b);
+
+    return 0;
+}
