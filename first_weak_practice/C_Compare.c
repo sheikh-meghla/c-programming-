4b6bee1ecaf28b1dac2313ea9576c508eadfe0cd
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-27 14:24:19 +0600

    Refactor C. Compare program to use strcmp for string comparison

--- first_weak_practice/C_Compare.c
+++ first_weak_practice/C_Compare.c
@@ -1,48 +1,73 @@
-#include<stdio.h>
-#include<stdbool.h>
-int main(){
+// #include<stdio.h>
+// #include<stdbool.h>
+// int main(){
 
-    char X[101], Y[101];
-    scanf("%s %s",&X,&Y);
+//     char X[101], Y[101];
+//     scanf("%s %s",&X,&Y);
 
-    int i=0;
+//     int i=0;
 
-    while (true)
-    {
-        if (X[i] == '\0' && Y[i] == '\0')
-        {
-            printf("%s\n",X);
-            break;
-        }
-        else if(X[i]== '\0')
-        {
-            printf("%s\n",X);
-            break;
-        }
-        else if (Y[i] == '\0')
-        {
-            printf("%s\n",Y);
-            break;
-        }
-        else if (X[i]<Y[i])
-        {
-            printf("%s\n",X);
-            break;
-        }
-        else if (X[i]>Y[i])
-        {
-            printf("%s\n",Y);
-            break;
-        }
-        else if (X[i]==Y[i])
-        {
-            i++;
-        }
+//     while (true)
+//     {
+//         if (X[i] == '\0' && Y[i] == '\0')
+//         {
+//             printf("%s\n",X);
+//             break;
+//         }
+//         else if(X[i]== '\0')
+//         {
+//             printf("%s\n",X);
+//             break;
+//         }
+//         else if (Y[i] == '\0')
+//         {
+//             printf("%s\n",Y);
+//             break;
+//         }
+//         else if (X[i]<Y[i])
+//         {
+//             printf("%s\n",X);
+//             break;
+//         }
+//         else if (X[i]>Y[i])
+//         {
+//             printf("%s\n",Y);
+//             break;
+//         }
+//         else if (X[i]==Y[i])
+//         {
+//             i++;
+//         }
         
         
         
-    }
+//     }
     
 
-    return 0;
+//     return 0;
+// }
+
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char X[101],Y[101];
+    scanf("%s %s",&X,&Y);
+
+    int val = strcmp(X,Y);
+
+    if (val<0)
+    {
+        printf("%s\n",X);
+    }
+
+    if (val>0)
+    {
+        printf("%s\n",Y);
+    }
+
+    if (val==0)
+    {
+        printf("%s\n",X);
+    }
+    
 }
