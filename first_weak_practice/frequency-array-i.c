2cbf64785ae563f14fc46509ca1d12e456c490cb
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-27 17:40:07 +0600

    Refactor frequency counting logic to eliminate conditional statements

--- first_weak_practice/frequency-array-i.c
+++ first_weak_practice/frequency-array-i.c
@@ -2,7 +2,9 @@
 int main(){
 
     int n;
+
     scanf("%d",&n);
+    
     int a[n];
 
     for (int i = 0; i < n; i++)
@@ -14,29 +16,16 @@ int main(){
     
     for (int i = 0; i < n; i++)
     {
-        if (a[i]==0)
-        {
-            fre[0]++;
-        }
-        else if (a[i]==1)
-        {
-            fre[1]++;
-        }
-        else if (a[i]==2)
-        {
-            fre[2]++;
-        }
-        else if (a[i]==3)
-        {
-            fre[3]++;
-        }
-        else if (a[i]==4)
-        {
-            fre[4]++;
-        }
+        int val = a[i];
+
+        fre[val]++;
         
     }
-    printf("%d %d %d %d %d \n",fre[0],fre[1],fre[2],fre[3],fre[4]);
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d -> %d\n",i,fre[i]);
+    }
+    
     
 
     return 0;
