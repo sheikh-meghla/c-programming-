ce9847d5a5519850a06991d9473b26d7c136955f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-28 19:50:16 +0600

    Refactor code structure for improved readability and maintainability

---
+++ first_weak_practice/flag-variable.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    int x;
+    scanf("%d",&x);
+
+    int flag = 0;
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if (a[i]+a[j] == x)
+            {
+                flag = 1;
+                printf("yes\n");
+                printf("%d %d\n",a[i],a[j]);
+                break;
+            }
+            
+        }               
+    }
+
+    if (flag == 0)
+        {
+            printf("no");
+        }   
+
+    return 0;
+}
