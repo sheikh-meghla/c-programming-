a9948bef20d386159dc8689e7818113b1442307d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-29 08:40:06 +0600

    Add rounding demonstration to useful math functions

--- first_weak_practice/useful-math-functions.c
+++ first_weak_practice/useful-math-functions.c
@@ -4,7 +4,8 @@
 int main(){
     int ans = ceil(3.1);
     int ans1 = floor(3.1);
+    int ans2 = round(3.5);
 
-    printf("%d %d",ans,ans1);
+    printf("%d %d %d",ans,ans1,ans2);
     return 0;
 }
