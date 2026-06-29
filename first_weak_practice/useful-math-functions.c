917d823a4efacc28abfc62f639d00f03c52c713d
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-29 08:44:25 +0600

    Add power function demonstration to useful math functions

--- first_weak_practice/useful-math-functions.c
+++ first_weak_practice/useful-math-functions.c
@@ -6,7 +6,8 @@ int main(){
     int ans1 = floor(3.1);
     int ans2 = round(3.5);
     int ans3 = sqrt(36);
+    int ans4 = pow(4,3);
 
-    printf("%d %d %d %d",ans,ans1,ans2,ans3);
+    printf("%d %d %d %d %d",ans,ans1,ans2,ans3,ans4);
     return 0;
 }
