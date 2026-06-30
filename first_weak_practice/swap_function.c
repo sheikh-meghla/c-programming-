d7f9706b9f214838e5e0aecac34d9e0b73c40013
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-30 19:56:07 +0600

    Add swap function example in C

---
+++ first_weak_practice/swap_function.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int x, y;
+
+    scanf("%d %d", &x, &y);
+
+    swap(&x, &y);
+
+    printf("%d %d", x, y);
+
+    return 0;
+}
