238f3fd57b4e73290504d2e37de49f99e01c4372
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-06-30 19:33:16 +0600

    Add function to demonstrate array usage in C

---
+++ first_weak_practice/function-with-array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+    void func(int a[], int n){
+    for(int i=0;i<n;i++)
+    {
+    printf("%d ",a[i]); 
+    }
+    }
+
+    int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]); 
+    }
+    func(a,n);
+    }
