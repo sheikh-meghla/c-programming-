255e02d29b7e40ba6f30db0a50590bb5a4afe604
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-03 12:17:29 +0600

    Add implementation of B. Print from 1 to N program using recursion

---
+++ first_weak_practice/B_Print_from_1_to_N.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fun(int n){
+    
+    if(n==0){
+        
+        return 0;
+
+    }
+
+
+    fun(n-1);
+    printf("%d\n",n);
+
+    
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    fun(n);
+}
