19ef5cb16451090d61eb8a712ec7249c79ad3029
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-04 11:40:29 +0600

    Add T. Matrix implementation for Codeforces problem

---
+++ first_weak_practice/T_Matrix.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include <stdlib.h>
+
+int main(){
+    int r,c,n;
+    scanf("%d",&n);
+    r = n;
+    c = n;
+    int primary_sum = 0;
+    int secondary_sum = 0;
+    int a[r][c];
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }        
+    }
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (i == j )
+            {
+                primary_sum += a[i][j];
+            }
+                            
+                
+        }           
+            
+    }
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (i + j == r-1 )
+            {
+                secondary_sum += a[i][j];   
+            }
+                            
+                
+        }           
+            
+    }
+    
+    int difference = primary_sum - secondary_sum;
+
+    printf("%d",abs(difference));
+        
+    
+    
+}
