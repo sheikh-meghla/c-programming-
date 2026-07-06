6c2642e603df4d0069b340d890593360cff64ee4
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-06 19:57:38 +0600

    Add implementation for Matrix Again problem

---
+++ first_weak_practice/Matrix_Again.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int m,n;
+    scanf("%d %d",&m,&n);
+    int a[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+        
+    }   for (int j = 0; j < n; j++)
+        {
+            printf("%d ", a[m-1][j]);
+        }
+        printf("\n");
+        
+        for (int i = 0; i < m; i++)
+        {
+        printf("%d ", a[i][n-1]);
+        }     
+        
+        
+    }
+    
