1697cde22c25e799d4d98d5168e76dd78714484b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-07 19:20:30 +0600

    Add implementation for Six Odd Numbers problem

---
+++ first_weak_practice/Six_Odd_Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = n; i < n+12; i++)
+    {
+        if (i % 2 != 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
