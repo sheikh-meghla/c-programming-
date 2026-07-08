4c7752aeaf485513159da52c559631ed788d588c
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-08 19:06:22 +0600

    Add implementation for Sum of Consecutive Odd Numbers II problem

---
+++ first_weak_practice/Sum_of_Consecutive_Odd_Numbers_II.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    while (n--)
+    {
+        int x, y;
+        scanf("%d %d", &x, &y);
+
+        if (x > y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        int sum = 0;
+
+        for (int i = x + 1; i < y; i++)
+        {
+            if (i % 2 != 0)
+            {
+                sum += i;
+            }
+        }
+
+        printf("%d\n", sum);
+    }
+
+    return 0;
+}
