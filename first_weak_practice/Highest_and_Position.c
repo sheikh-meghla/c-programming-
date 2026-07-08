613babcd6221b4ed269eb0c17bd8bb100b441bef
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-08 15:43:56 +0600

    Add implementation for Weighted Averages problem

---
+++ first_weak_practice/Highest_and_Position.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100];
+
+    for (int i = 0; i < 100; i++)
+    {
+        if (condition)
+        {
+            /* code */
+        }
+        
+
+        printf("%.1f\n", avg);
+    }
+
+    return 0;
+}
---
+++ first_weak_practice/Weighted_Averages.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    float a, b, c, avg;
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%f %f %f", &a, &b, &c);
+
+        avg = (a * 2 + b * 3 + c * 5) / 10.0;
+
+        printf("%.1f\n", avg);
+    }
+
+    return 0;
+}
