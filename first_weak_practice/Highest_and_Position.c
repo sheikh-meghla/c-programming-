2c5e4b11313d4f168054a3524c06c0ec5cff9c6f
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-08 15:52:06 +0600

    Fix logic for finding highest number and its position in array

--- first_weak_practice/Highest_and_Position.c
+++ first_weak_practice/Highest_and_Position.c
@@ -3,17 +3,26 @@
 int main()
 {
     int a[100];
+    int highest, position;
 
     for (int i = 0; i < 100; i++)
     {
-        if (condition)
-        {
-            /* code */
-        }
-        
+        scanf("%d", &a[i]);
 
-        printf("%.1f\n", avg);
+        if (i == 0)
+        {
+            highest = a[i];
+            position = 1;
+        }
+        else if (a[i] > highest)
+        {
+            highest = a[i];
+            position = i + 1;
+        }
     }
+
+    printf("%d\n", highest);
+    printf("%d\n", position);
 
     return 0;
 }
