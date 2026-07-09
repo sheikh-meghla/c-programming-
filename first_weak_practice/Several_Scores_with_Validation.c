81ae810474108fc5c76df9a3ce1e8fb4fe884dce
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-07-09 09:09:37 +0600

    Refactor Several Scores with Validation to improve structure and readability

--- first_weak_practice/Several_Scores_with_Validation.c
+++ first_weak_practice/Several_Scores_with_Validation.c
@@ -1,32 +1,60 @@
 #include<stdio.h>
 int main(){
 
+
+    while(1) {
+
+        
     int count = 0;
     float average = 0;
     float sum = 0;
+    
+        while (count < 2)
+        {
+            float n;
+            scanf("%f",&n);
+            if (n >= 0 && n <= 10)
+            {
+                sum += n;
+                count++;
+            }
+            else
+            {
+                printf("nota invalida\n");
+            }
+            if (count == 2)
+            {
+                break;
+            }
+            
+            
+        }
 
-    while (1)
-    {
-        float n;
-        scanf("%f",&n);
-        if (n >= 0 && n <= 10)
-        {
-            sum += n;
-            count++;
+        average = sum / 2.0;
+        printf("media = %.2f\n",average);
+
+        while(1) {
+
+            printf("novo calculo (1-sim 2-nao)\n");
+
+            int option;
+            scanf("%d",&option);
+
+                if(option == 1){
+                    break;
+                }
+
+                if(option == 2){
+                    return 0;
+                }
         }
-        else
-        {
-            printf("nota invalida\n");
-        }
-        if (count == 2)
-        {
-            break;
-        }
+
+
         
-        
+
     }
-    average = sum / 2.0;
-    printf("media = %.2f\n",average);
+
+
 
     return 0;
 }
